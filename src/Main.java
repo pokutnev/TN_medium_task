949f9326b300c67b8ea8b4a2ac04b53a413df456
Anton Pokutnev <pokutnev02@gmail.com>
2023-06-26 11:37:56 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Node root = new Node(5);
+
+        root.setLeftChild(new Node(3));
+        root.getLeftChild().setLeftChild(new Node(2));
+        root.getLeftChild().setRightChild(new Node(4));
+
+        root.setRightChild(new Node(7));
+        root.getRightChild().setLeftChild(new Node(6));
+        root.getRightChild().setRightChild(new Node(8));
+
+        Postorder T = new Postorder();
+        T.getTreeElements(root);
+
+    }
+}
