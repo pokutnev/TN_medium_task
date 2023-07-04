14ef9876be16b35763fdf2c8c90eaf8b3cb6a22e
Anton Pokutnev <pokutnev02@gmail.com>
2023-07-04 16:45:49 +0300

    add test

--- src/main/ListCalculator.java
+++ src/main/ListCalculator.java
@@ -1,18 +1,19 @@
 package main;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 public class ListCalculator {
 
-    public static int getPositiveSum(List<Integer> list) {
-
-        return list.stream().filter(number -> number > 0).reduce(0, Integer::sum);
-
+    public static int getPositiveSum(List<Integer> sum) {
+        return sum.stream().
+                filter(number -> number > 0).
+                reduce(0, Integer::sum);
     }
 
-    public static int getNegativeSum(List<Integer> list) {
-        return list.stream().filter(number -> number < 0).reduce(0, Integer::sum);
+    public static int getNegativeSum(List<Integer> sum) {
+        return sum.stream().
+                filter(number -> number < 0).
+                reduce(0, Integer::sum);
     }
 
 }
--- src/test/ListCalculatorTest.java
+++ src/test/ListCalculatorTest.java
@@ -2,6 +2,7 @@ package test;
 
 import main.ListCalculator;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 class ListCalculatorTest {
 
     @Test
+    @DisplayName("test method getting sum of positive numbers")
     void getPositiveSum() {
 
         List<Integer> list = new ArrayList<>();
@@ -22,15 +24,13 @@ class ListCalculatorTest {
         list.add(-1);
 
         int expectedResult = 14;
-
         int positiveSum = ListCalculator.getPositiveSum(list);
 
         Assertions.assertEquals(expectedResult, positiveSum);
-
-
     }
 
     @Test
+    @DisplayName("test method getting sum of negative numbers")
     void getNegativeSum() {
 
         List<Integer> list = new ArrayList<>();
@@ -43,12 +43,9 @@ class ListCalculatorTest {
         list.add(-1);
 
         int expectedResult = -11;
-
         int negativeSum = ListCalculator.getNegativeSum(list);
 
         Assertions.assertEquals(expectedResult, negativeSum);
-
-
-
     }
+
 }
