c431e92d2538568fa1ff091082fcc2c1e67fd3b7
Anton Pokutnev <pokutnev02@gmail.com>
2023-07-04 13:34:46 +0300

    add tests

--- src/main/DuplicateElements.java
+++ src/main/DuplicateElements.java
@@ -5,23 +5,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class DuplicateElements{
+public class DuplicateElements {
 
-    public static List<Integer> getDuplicates(List<Integer> list){
+    public static List<Integer> getDuplicates(List<Integer> sequenceOfElements) {
 
-        Map<Integer, Integer> mapA = new HashMap<>();
+        Map<Integer, Integer> uniqueElements = new HashMap<>();
 
-        for (Integer i : list) {
-            mapA.merge(i, 1, Integer::sum);
+        for (Integer i : sequenceOfElements) {
+            uniqueElements.merge(i, 1, Integer::sum);
         }
 
         List<Integer> duplicateElements = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> i : mapA.entrySet()) {
-            if(i.getValue() > 1){
+
+        for (Map.Entry<Integer, Integer> i : uniqueElements.entrySet()) {
+            if (i.getValue() > 1) {
                 duplicateElements.add(i.getKey());
             }
         }
-
 
         return duplicateElements;
     }
--- src/test/DuplicateElementsTest.java
+++ src/test/DuplicateElementsTest.java
@@ -2,16 +2,16 @@ package test;
 
 import main.DuplicateElements;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.List;
-
 
 class DuplicateElementsTest {
 
     @Test
-    void getDuplicatesTest() {
+    @DisplayName("test method for finding duplicate elements and getting a list with duplicates")
+    void getDuplicatesReturnListWithDuplicates() {
 
         List<Integer> list = new ArrayList<>();
 
@@ -26,11 +26,29 @@ class DuplicateElementsTest {
         list.add(3);
 
         List<Integer> expectedResult = List.of(1, 3, 4);
-
         List<Integer> resultList = DuplicateElements.getDuplicates(list);
 
         Assertions.assertEquals(expectedResult, resultList);
-
-
     }
+
+    @Test
+    @DisplayName("test method for finding duplicate elements and getting empty list")
+    void getDuplicatesReturnListWithoutDuplicates() {
+
+        List<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+
+        List<Integer> expectedResult = List.of();
+        List<Integer> resultList = DuplicateElements.getDuplicates(list);
+
+        Assertions.assertEquals(expectedResult, resultList);
+    }
+
 }
