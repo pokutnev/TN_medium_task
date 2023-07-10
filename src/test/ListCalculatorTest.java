2ba8d4f42bb2c44787791985af5ee18ae1020d80
Anton Pokutnev <pokutnev02@gmail.com>
2023-07-10 18:52:57 +0300

    rename variables

--- src/test/ListCalculatorTest.java
+++ src/test/ListCalculatorTest.java
@@ -1,11 +1,12 @@
 package test;
 
 import main.ListCalculator;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class ListCalculatorTest {
@@ -14,40 +15,40 @@ class ListCalculatorTest {
     @DisplayName("test method getting sum of positive numbers")
     void getPositiveSum() {
 
-        List<Integer> list = new ArrayList<>();
+        List<Integer> sequenceOfElements = new ArrayList<>();
 
-        list.add(5);
-        list.add(5);
-        list.add(-2);
-        list.add(4);
-        list.add(-8);
-        list.add(-1);
+        sequenceOfElements.add(5);
+        sequenceOfElements.add(5);
+        sequenceOfElements.add(-2);
+        sequenceOfElements.add(4);
+        sequenceOfElements.add(-8);
+        sequenceOfElements.add(-1);
 
         int expectedResult = 14;
-        ListCalculator CalculatorObject = new ListCalculator();
-        int positiveSum = CalculatorObject.getPositiveSum(list);
+        var calculatorObject = new ListCalculator();
+        int positiveSum = calculatorObject.getPositiveSum(sequenceOfElements);
 
-        Assertions.assertEquals(expectedResult, positiveSum);
+        assertEquals(expectedResult, positiveSum);
     }
 
     @Test
     @DisplayName("test method getting sum of negative numbers")
     void getNegativeSum() {
 
-        List<Integer> list = new ArrayList<>();
+        List<Integer> sequenceOfElements = new ArrayList<>();
 
-        list.add(5);
-        list.add(5);
-        list.add(-2);
-        list.add(4);
-        list.add(-8);
-        list.add(-1);
+        sequenceOfElements.add(5);
+        sequenceOfElements.add(5);
+        sequenceOfElements.add(-2);
+        sequenceOfElements.add(4);
+        sequenceOfElements.add(-8);
+        sequenceOfElements.add(-1);
 
         int expectedResult = -11;
-        ListCalculator CalculatorObject = new ListCalculator();
-        int negativeSum = CalculatorObject.getNegativeSum(list);
+        var calculatorObject = new ListCalculator();
+        int negativeSum = calculatorObject.getNegativeSum(sequenceOfElements);
 
-        Assertions.assertEquals(expectedResult, negativeSum);
+        assertEquals(expectedResult, negativeSum);
     }
 
 }
